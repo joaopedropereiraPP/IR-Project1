c00731cb77e35aac128241aee262c866f11dcc2c
pedropereira96 <jpspjpsp96@gmail.com>
2021-11-16 20:48:04 +0000

    Some changes in the main class

--- src/main.py
+++ src/main.py
@@ -13,29 +13,22 @@ class Main:
 
 
         #Default arguments
-        self.data_path = ['path1' , 'path2', 'path3', 'path4']
+        self.data_path = ['content/amazon_reviews_us_Books_v1_00.tsv.gz' , 
+                            'content/amazon_reviews_us_Digital_Music_Purchase_v1_00.tsv.gz', 
+                            'content/amazon_reviews_us_Digital_Video_Games_v1_00.tsv.gz', 
+                            'content/amazon_reviews_us_Music_v1_00.tsv.gz']
         self.use_stopwords = True
         self.stopwords_path = 'content/stopwords.txt'
         self.minimum_word_size = 3
         self.use_word_size = True
-        self.use_stemmer = True
+        self.stemmer_enabled = True
 
         self.parser = ArgumentParser()
-
-       # self.tokenizer = Tokenizer(stopwords_path)
-        #self.indexer = Indexer()
-        #self.size=0
         
+        self.tokenizer = Tokenizer(stopwords_path = self.stopwords_path, stemmer_enabled = self.stemmer_enabled, size_filter=self.minimum_word_size)
+        
+        self.indexer = Indexer(tokenizer = self.tokenizer)
     
-    def gen_chunks(self, reader, chunksize):
-        """ SOME HELP: https://stackoverflow.com/questions/4956984/how-do-you-split-reading-a-large-csv-file-into-evenly-sized-chunks-in-python """
-        chunk = []
-        for i, line in enumerate(reader):
-            if (i % chunksize == 0 and i > 0):
-                yield chunk
-                del chunk[:]  # or: chunk = []
-            chunk.append(line)
-        yield chunk
 
     def process(self, chunksize = 2) :
         """All the process start here"""
@@ -47,62 +40,31 @@ class Main:
         #original_file = "content/amazon_reviews_us_Digital_Video_Games_v1_00.tsv.gz"
         original_file = "../content/data.tsv.gz"
         
-        
+        print(self.data_path)
         for file in self.data_path:
-            with open(file, "rt") as tsv_file:
-                reader = DictReader(tsv_file, delimiter="\t")
-                
-                
-                for chunk in self.gen_chunks(reader, chunksize):
-                    print(" \tNEW CHUNK")
-                    tokens = []
-
-                    #Tokenizer for every chunks
-                    for row in chunk:
-                        
-                        identification = row['review_id']
-                        text_value = row['product_title']  + " " + row['review_headline']  + " " + row['review_body'] 
-                        print(text_value)
-                        tokens += self.tokenizer.tokenize(text_value)
-                        print(tokens)
-                    """
-                    1º Realizar index para o bloco de tokens do chunk (pedaço)
-                    2º Criar bloco (ficheiro) e escrever ( term: postings )
-                        Ou seja, vai ser criado
-                    """
-
-        
-                #tokens = sorted(tokens)
-                #Indexer
-                #self.indexer.index(row['review_id'],tokens)
-                #print("------------####################-----------------")
-                #for x in self.indexer.get_indexed().keys():
-                    #print(x)
-                    """print(type(self.indexer.get_indexed()[x]['posting_list']))
-                    print("documents_frequency: "+ str(self.indexer.get_indexed()[x]['documents_frequency']))
-                    """
-                print("*************************************************")
+            self.indexer.index_data_source(data_source_path = file)
+         
  
     def parse_args(self):
         
         parser = ArgumentParser()
         #PATH TO NEW DATA FILE
-        parser.add_argument("--data_path", help="increase output verbosity", 
-                            type=str, metavar="(path to data file)")
+        parser.add_argument("--data_path", help="Set the path to the data", 
+                            type=str, metavar="(path to data file (.gz))")
         #NOT USE STOPWORDS LIST
-        parser.add_argument("--nostopwords", help="Define to not use Stop Words List",
+        parser.add_argument("--nostopwords", help="Set not to use Stop Words List",
                             action="store_false")
         #PATH TO NEW STOPWORDS
-        parser.add_argument("--stopwords", help="Define path to Stop Words List", 
+        parser.add_argument("--stopwords", help="Set the path to Stop Words List", 
                             type=str, metavar="(path to stopwords list)")
         #MINIMUM WORD SIZE
-        parser.add_argument("--word_size", help="Define minimum words size", 
-                            type=str, metavar="(integer number)")
+        parser.add_argument("--word_size", help="Set the minimum words size", 
+                            type=int, metavar="(integer number)")
         #NO MINIMUM WORD SIZE
-        parser.add_argument("--no_word_size", help="Define minimum words size",
+        parser.add_argument("--no_word_size", help="Set not to use minimum words size",
                             action="store_false")
         #NOT USE STEMMER APPROACH
-        parser.add_argument("--nostemmer", help="increase output verbosity",
+        parser.add_argument("--nostemmer", help="Set not to use Stemmer",
                             action="store_false")
 
         return parser
@@ -111,46 +73,37 @@ class Main:
 
         if args.data_path :
             self.data_path = [args.data_path]
-            if not path.isfile(self.data_path[0]):
-                print("File does not exist")
+            if not path.isfile(self.data_path[0]) or not self.data_path[0].endswith('.gz'):
+                print("File does not exist or does not have the correct extension! ")
                 print(parser.parse_args(['-h']))
                 sys.exit()
         
-
         #SE NÃO QUER LISTA DE STOPWORS, MAS DEFINE UMA LISTA
         if (not args.nostopwords) and (args.stopwords != None):
             print(parser.parse_args(['-h']))
             sys.exit()
 
-        
         if not args.nostopwords:
             self.use_stopwords = False
             self.stopwords_path = ''
 
-
-        
         if args.stopwords:
             self.stopwords_path = args.stopwords
-
 
         #SE NÃO QUER TAMANHO MINIMO DE LETRAS, MAS DEFINE UM TAMANHO
         if (not args.no_word_size) and (args.word_size != None):
             print(parser.parse_args(['-h']))
             sys.exit()
-
         
         if args.word_size:
             self.minimum_word_size = args.word_size
-
-        
+ 
         if not args.no_word_size:
             self.use_word_size = False
             self.minimum_word_size = 0
         
-        
         if not args.nostemmer:
-            self.use_stemmer = False
-
+            self.stemmer_enabled = False
 
     def main(self):
 
@@ -165,12 +118,11 @@ class Main:
         print("stopwords_path: " + str(self.stopwords_path))
         print("minimum_word_size: " + str(self.minimum_word_size))
         print("use_word_size: " + str(self.use_word_size))
-        print("use_stemmer: " + str(self.use_stemmer))
+        print("stemmer_enabled: " + str(self.stemmer_enabled))
         
 
-        process = self.process()
+        self.process()
 
-        #process.initilize(use_size_filter, tokenizer_length, stemmer)
 
 
 
