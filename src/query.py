fb62e21f89c086c8e8468ddf41094447511fc9ce
Pedro <pedro.jp@ua.pt>
2021-12-21 13:44:24 +0000

    update read_configurations to read ini file and insert bm25 searcher

--- src/query.py
+++ src/query.py
@@ -4,6 +4,7 @@ from os import path
 from collections import defaultdict
 from bm25 import bm25
 from time import time
+from configparser import ConfigParser
 
 class Query:
     doc_keys = {}
@@ -21,7 +22,8 @@ class Query:
         #path files
         self.doc_keys_folder_path = ('index/'  + path.basename(data_path).split('.')[0]  + '/DocKeys.tsv')
         self.master_index_folder_path = ('index/' + path.basename(data_path).split('.')[0] + '/MasterIndex.tsv')
-        self.configurations_folder_path = ('index/' + path.basename(data_path).split('.')[0] + '/conf.tsv')
+        self.posting_index_block_file = 'index/'+ path.basename(data_path).split('.')[0]+'/PostingIndexBlock{}.tsv'
+        self.configurations_folder_path = ('index/' + path.basename(data_path).split('.')[0] + '/conf.ini')
 
 
         self.doc_keys= (defaultdict(lambda: defaultdict(lambda: [])))
@@ -49,27 +51,23 @@ class Query:
         #update master_index
         self.read_master_index()
 
-    def process_query(self):
+        self.files_to_open = {}
 
-        print("Method used: {}".format(self.index_type))
-        print("Words to search:")
-        to_search = input()
         
+    def process_query(self, to_search):
+        #conjunto de palavras
+        terms = self.term_tokenizer(to_search)
+        start_time = time()
+        if self.index_type == 'lnc.ltc':
+            pass 
 
-        while ( to_search != '0'):
-            
-            #conjunto de palavras
-            terms = self.term_tokenizer(to_search)
-            start_time = time()
-            if self.index_type == 'lnc.ltc':
-                pass 
-            elif self.index_type == 'bm25':
-                bm25_method = bm25(data_path=self.data_path, doc_keys = self.doc_keys, master_index=self.master_index)
-                bm25_method.bm25_search(terms)
-            total_time = time() - start_time
-            print("Time used: {:0.3f}s".format(total_time))
-            print("Words to search:")
-            to_search = input()
+        elif self.index_type == 'bm25':
+
+            self.bm25_search(terms)
+
+        total_time = time() - start_time
+        print("Time used: {:0.3f}s".format(total_time))
+
 
 
 
@@ -79,7 +77,7 @@ class Query:
             for row in filecontent:
                 self.doc_keys[row[0]]['real_id'] = row[1]
                 self.doc_keys[row[0]]['doc_name'] = row[2]
-                self.doc_keys[row[0]]['doc_len'] = row[3]
+                #self.doc_keys[row[0]]['doc_len'] = row[3]
 
     def read_master_index(self):
         with open(self.master_index_folder_path, 'r') as file:
@@ -92,16 +90,86 @@ class Query:
                 self.master_index[term]['file_path'] = file_path
 
     def read_configurations(self):
-        with open(self.configurations_folder_path, 'r') as file:
-            filecontent = list(csv.reader(file, delimiter='\t'))
-            self.index_type = filecontent[0][1]
-            self.size_filter = int(filecontent[1][1])
-            self.stemmer_enabled = True if filecontent[2][1] == 'True' else False
-            self.stopwords_path = filecontent[3][1]
-            self.use_positions = True if filecontent[4][1] == 'True' else False
+        config = ConfigParser()
+        config.read(self.configurations_folder_path)
+        self.index_type = config['METADATA']['index_type']
+        self.size_filter = int( config['METADATA']['size_filter'] )
+        self.stemmer_enabled = True if config['METADATA']['stemmer_enabled'] == 'True' else False
+        self.stopwords_path = config['METADATA']['stopwords_path']
+        self.use_positions = True if config['METADATA']['use_positions'] == 'True' else False
 
     def term_tokenizer(self, term):
         if self.use_positions:
             return self.tokenize.tokenize_positional(input_string = term)
         else:
             return self.tokenize.tokenize(input_string = term)
+
+
+
+###  BEST MATCH 25
+    def bm25_search(self, terms):
+        self.post_data = {}
+        
+        bm25_ranking = {}
+
+        #Stores all documents with their terms to optimize the search within the PostingIndexBlock file 
+        self.store_files_to_open(terms)
+
+        for file_number in self.files_to_open.keys():
+            
+            file_name = self.posting_index_block_file.format(file_number)
+            self.read_posting_index_block(file_name)
+
+            index = 0
+            for terms_on_file in self.files_to_open[file_number]:
+
+                for any_term in terms_on_file.keys():
+                    for docs in self.post_data[any_term].keys():
+                        if docs not in bm25_ranking:
+                            bm25_ranking[docs] = 0
+                            idf = float(self.files_to_open[file_number][index][any_term]['idf'])
+                            weight = float(self.post_data[any_term][docs])
+                            count = int(self.files_to_open[file_number][index][any_term]['count'])
+                        bm25_ranking[docs] += ((weight * idf) * count)
+                    index += 1
+        bm25_ranking = sorted(bm25_ranking.items(), key=lambda x: x[1], reverse=True) 
+        for i in range(0,10):
+            if len(bm25_ranking) >= i + 1:
+                doc=self.doc_keys[tuple(list(bm25_ranking)[i])[0]]['real_id']
+                doc_name=self.doc_keys[tuple(list(bm25_ranking)[i])[0]]['doc_name']
+                print("{}º {} -> {}".format(i, doc, doc_name))
+
+
+
+    def store_files_to_open(self, terms):
+        term_result={}
+        result={}
+        for term in terms.keys():
+            term_size = len(terms[term])
+            doc = self.master_index[term]['file_path']
+            idf = self.master_index[term]['idf']
+
+            term_result={}
+            result={}
+            result['idf'] = idf
+            result['count'] = term_size
+            
+            term_result[term] = result
+            if doc not in self.files_to_open:
+                self.files_to_open[doc] = []
+
+            self.files_to_open[doc].append(term_result)
+
+    def read_posting_index_block(self, file_to_analyse):
+        self.post_data = {}
+        with open(file_to_analyse, 'r') as file:
+                    filecontent = csv.reader(file, delimiter='\t')
+                    for a in filecontent:
+                        term = a[0]
+                        post = {}
+                        for n in range(1, len(a)):
+                            values = a[n].split(":")
+                            doc_id = values[0]
+                            weight = values[1]
+                            post[doc_id] = weight
+                        self.post_data[term] = post
