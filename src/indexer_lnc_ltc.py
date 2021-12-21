d18e321362214c1414de0b1a5f42e02b47c2fea3
IvoFelix <ivo.felix@sapo.pt>
2021-12-21 22:00:55 +0000

    Corrected weight calculation for IndexerLncLtc.

--- src/indexer_lnc_ltc.py
+++ src/indexer_lnc_ltc.py
@@ -19,34 +19,32 @@ class IndexerLncLtc(Indexer):
 
     def create_postings(self, doc_id: int, doc_body: str) -> None:
         tokens = self.tokenizer.tokenize_positional(doc_body)
+        Wtdnorm = 0
         for token in tokens:
             tf = len(tokens[token])
-            Wtd = 1 + self.log(tf)
+            Wtdnorm += (1 + self.log(tf)) ** 2
+        self.doc_keys[doc_id].append(str(Wtdnorm))
+        for token in tokens:
+            tf = len(tokens[token])
+            Wtd = (1 + self.log(tf)) / sqrt(Wtdnorm)
             self.inverted_index[token].append(PostingWeighted(doc_id, Wtd))
             self.nr_postings += 1
             self.block_posting_count += 1
 
     def create_postings_positional(self, doc_id: int, doc_body: str) -> None:
         tokens = self.tokenizer.tokenize_positional(doc_body)
+        Wtdnorm = 0
         for token in tokens:
             tf = len(tokens[token])
-            Wtd = 1 + self.log(tf)
+            Wtdnorm += (1 + self.log(tf)) ** 2
+        self.doc_keys[doc_id].append(str(Wtdnorm))
+        for token in tokens:
+            tf = len(tokens[token])
+            Wtd = (1 + self.log(tf)) / sqrt(Wtdnorm)
             self.inverted_index[token].append(
                 PostingWeightedPositional(doc_id, Wtd, tokens[token]))
             self.nr_postings += 1
             self.block_posting_count += 1
-
-    def final_indexing_calculations(self) -> None:
-        self.calculate_weights()
-
-    def calculate_weights(self) -> None:
-        for term in self.inverted_index:
-            Wtdnorm = 0
-            for posting in self.inverted_index[term]:
-                Wtdnorm += posting.weight * posting.weight
-            for posting in self.inverted_index[term]:
-                normalized_weight = sqrt(Wtdnorm)
-                posting.weight = posting.weight / normalized_weight
 
     def posting_list_from_str(self,
                               posting_str_list: str) -> List[PostingWeighted]:
